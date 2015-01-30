886cf7196cae6e27404da190caad9b668cc9d011
Abhimanyu <arya.abhimanyu.92@gmail.com>
2015-01-30 12:25:23 +0530

    modified the spec to incorporate the new_square method

--- GeometryShapes/spec/geometry/rectangle_spec.rb
+++ GeometryShapes/spec/geometry/rectangle_spec.rb
@@ -3,13 +3,24 @@ require 'spec_helper'
 describe Rectangle do
 
 let(:rectangle1) {Rectangle.new(12, 10)} 
+let(:square) {Rectangle.new_Square(10)}
 
-  it 'calculates perimeter' do
+  it 'calculates perimeter of rectangle' do
     expect(rectangle1.perimeter).to eq(44)
   end
 
-  it 'calculates area' do
+  it 'calculates area of rectangle' do
   	expect(rectangle1.area).to eq(120)
   end
 
+  	it 'calculates perimeter of square' do
+    expect(square.perimeter).to eq(40)
+  end
+
+  it 'calculates area of square' do
+  	expect(square.area).to eq(100)
+  end
+
+
 end
+
